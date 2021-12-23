3ad93ccaff3882d9678c20784cb572232081099d
Cristian Montecinos <52111100+titanmon6@users.noreply.github.com>
2021-12-23 20:01:42 -0300

    Add files via upload

---
+++ template/cabecera.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Inventario Bazar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+  </head>
+  <body>
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <ul class="nav navbar-nav"> 
+    <div class="container">
+    <div class="row">
+        <div class="col-4 mx-auto">
+    <img src="img/tanush.png" >    
+    
+    </ul>
+    </div>
+    </div>
+</div>
+</nav>
+</body>
+<br/>
